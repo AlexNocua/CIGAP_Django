85e0f6f09885f7f4b42a7974646847e45e9dcbbd
AlexNocua <nocua68@gmail.com~>
2024-08-01 00:02:27 -0500

    Asignación de las rutas de las aplicaciones en el archivo settings.py, así mismo comprobación del correcto funcionamiento de esas urls por medio de vistas creadas para cada aplicación mostrando una respuesta HTTP de que esta funcionando.

--- plataform_CIGAP/director/views.py
+++ plataform_CIGAP/director/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponsePermanentRedirect
 
 # Create your views here.
+
+
+def funcionando(request):
+    return HttpResponse('app_ director funcionando.')
--- plataform_CIGAP/estudiante/views.py
+++ plataform_CIGAP/estudiante/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponsePermanentRedirect
 
 # Create your views here.
+def funcionando (request):
+    return HttpResponse('app_ estudiante funcionando.')
