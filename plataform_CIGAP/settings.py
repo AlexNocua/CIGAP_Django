c5157657321b2a4e5f79417a9312b7a103b669ad
AlexNocua <nocua68@gmail.com~>
2024-10-12 23:19:26 -0500

    ajustes para produccion 12

--- plataform_CIGAP/settings.py
+++ plataform_CIGAP/settings.py
@@ -36,7 +36,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "0").lower() in ["true", "t", "1"]
+# DEBUG = os.getenv("DEBUG", "0").lower() in ["true", "t", "1"]
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 # ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
