09a079941588b78b8562d6f432b25c5c2a582012
AlexNocua <nocua68@gmail.com~>
2024-10-14 22:07:37 -0500

    Ajustes deploy 30

--- plataform_CIGAP/settings.py
+++ plataform_CIGAP/settings.py
@@ -21,7 +21,7 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # cargar la variable de entorno
-# load_dotenv()
+load_dotenv()
 
 
 def base_dir():
@@ -32,7 +32,7 @@ def base_dir():
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
