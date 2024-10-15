8c031c2845f0d622788716ba626e1cde8fdbbe05
AlexNocua <nocua68@gmail.com~>
2024-10-14 22:43:52 -0500

    Ajustes deploy 34

--- plataform_CIGAP/settings.py
+++ plataform_CIGAP/settings.py
@@ -32,7 +32,7 @@ def base_dir():
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
