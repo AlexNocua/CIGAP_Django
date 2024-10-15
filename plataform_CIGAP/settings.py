be39612b462c46989003abd7d49483316c30fa44
AlexNocua <nocua68@gmail.com~>
2024-10-14 22:33:48 -0500

    Ajustes deploy 33

--- plataform_CIGAP/settings.py
+++ plataform_CIGAP/settings.py
@@ -114,7 +114,7 @@ WSGI_APPLICATION = "plataform_CIGAP.wsgi.application"
 # Configurar la base de datos
 DATABASES = {
     'default': dj_database_url.config(
-        default=config("DATABASE_URL"),  # Carga la URL de la base de datos desde .env
+        default=os.getenv("DATABASE_URL"),  # Carga la URL de la base de datos desde .env
         conn_max_age=600,
         conn_health_checks=True,
     )
